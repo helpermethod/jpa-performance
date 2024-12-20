9b6154b0fd169fa8c2cc5ceae47e639383ff7ec6
Oliver Weiler <oliver@sdkman.io>
2024-12-20 13:40:45 +0100

    chore: Use @EntityGraph to initalisze lazy association

--- src/main/kotlin/io/helpermethod/jpa/performance/posts/CommentRepository.kt
+++ src/main/kotlin/io/helpermethod/jpa/performance/posts/CommentRepository.kt
@@ -3,9 +3,8 @@ package io.helpermethod.jpa.performance.posts
 import java.util.UUID
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
 interface CommentRepository : JpaRepository<Comment, UUID> {
-    @Query("from Comment c join fetch c.post")
+    @EntityGraph(attributePaths = ["post"])
     fun findCommentById(id: UUID): Comment
 }
